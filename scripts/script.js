b18d24d5c83500b75c54a716d33491ca9e59b098
Pravs <pravallic6@gmail.com>
2022-04-26 15:19:39 +0530

    First Commit

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+
+
+
+function menuClick() {
+    var x = document.getElementById("myNav");
+    if (x.className == "nav") {
+        x.className += " responsive";
+    } else {
+        x.className = "nav";
+    }
+}
